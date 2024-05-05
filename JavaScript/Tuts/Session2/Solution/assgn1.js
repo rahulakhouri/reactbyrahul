97b99c3a0d97c2c1a6a2d6edabe84e9c8dc3fa4c
RAHUL AKHOURI <59613880+rahulakhouri@users.noreply.github.com>
2024-05-05 21:02:47 +0530

    Update assgn1.js

--- JavaScript/Tuts/Session2/Solution/assgn1.js
+++ JavaScript/Tuts/Session2/Solution/assgn1.js
@@ -1,1 +1,48 @@
+<script>
 
+var amount = 20000
+
+//After one year
+amount = amount * 2
+
+//After 2nd year
+amount = amount * 2
+
+
+//After 3rd year
+amount = amount * 2
+  
+//After 4th year
+amount = amount * 2
+
+//After 5th year
+amount = amount * 2
+
+document.write("Amount after 5 yrs if it double every year --  ")
+
+document.write(amount)
+
+//After 6th year
+amount = amount * 2
+
+  
+//After 7th year
+amount = amount * 2
+
+//After 8th year
+amount = amount * 2
+
+
+//After 9th year
+amount = amount * 2
+
+  
+//After 10th year
+amount = amount * 2
+
+  
+document.write("Amount after 10 yrs if it double every year --  ")
+
+document.write(amount)
+
+</script>
